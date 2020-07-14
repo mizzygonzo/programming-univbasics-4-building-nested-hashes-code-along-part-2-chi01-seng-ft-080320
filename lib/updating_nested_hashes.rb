d518b91d6f866d32b99c1ec07250971307c4040b
MaryElizabeth Gonzales <xeligon@gmail.com>
2020-07-14 16:41:32 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -1,5 +1,6 @@
 def update_status
-   epic_tragedy = {
+   epic_tragedy[:montague] 
+   
       :montague => {
          :patriarch => {name: "Lord Montague", age: "53"},
          :matriarch => {name: "Lady Montague", age: "54"},
