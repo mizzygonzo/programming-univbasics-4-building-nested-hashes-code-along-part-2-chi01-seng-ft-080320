e44bbf00f432292e92507557a2c4699de19ab660
MaryElizabeth Gonzales <xeligon@gmail.com>
2020-07-14 16:51:16 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -1,24 +1,22 @@
 def update_status
-   epic_tragedy[:montague][:hero][:status] = "dead"
-    # => {
-    #     :patriarch => {name: "Lord Montague", age: "53"},
-    #     :matriarch => {name: "Lady Montague", age: "54"},
-    #     :hero => {name: "Romeo", age: "15", status: "alive"},
-    #     :hero_friends => [
-    #        {name: "Benvolio", age: "17", attitude: "worried"},
-    #        {name: "Mercutio", age: "18", attitude: "hot-headed"}
+   epic_tragedy => {[:montague][:hero][:status] = "dead"
+         :patriarch => {name: "Lord Montague", age: "53"},
+         :matriarch => {name: "Lady Montague", age: "54"},
+         :hero => {name: "Romeo", age: "15", status: "alive"},
+         :hero_friends => [
+            {name: "Benvolio", age: "17", attitude: "worried"},
+            {name: "Mercutio", age: "18", attitude: "hot-headed"}
          ]
       },
-      [:capulet][:heroine][:status] = "dead" 
-   #   => {
-  #       :patriarch => {name: "Lord Capulet", age: "50"},
-   #      :matriarch => {name: "Lady Capulet", age: "51"},
-    #     :heroine => {name: "Juliet", age: "15", status: "alive"},
-    #     :heroine_friends => [
-     #    {name: "Steven", age: "30", attitude: "confused"},
-    #     {name: "Nurse", age: "44", attitude: "worried"}
-    #     ]
-   #   }
+      => {[:capulet][:heroine][:status] = "dead" 
+         :patriarch => {name: "Lord Capulet", age: "50"},
+         :matriarch => {name: "Lady Capulet", age: "51"},
+         :heroine => {name: "Juliet", age: "15", status: "alive"},
+         :heroine_friends => [
+         {name: "Steven", age: "30", attitude: "confused"},
+         {name: "Nurse", age: "44", attitude: "worried"}
+         ]
+      }
    
 
   # Write code that changes the status of both Romeo and Juliet from "alive" to "dead"
